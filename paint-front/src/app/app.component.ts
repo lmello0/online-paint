977e8adf441baf6933a101542c5aa111a82f12c5
Lucas Mello dos Santos <contact@lmello.dev>
2025-02-02 13:45:04 -0300

    :sparkles: feat: add canvas

--- paint-front/src/app/app.component.ts
+++ paint-front/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { CanvasComponent } from './canvas/canvas.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ColorPickerComponent, ToolbarComponent],
+  imports: [ColorPickerComponent, ToolbarComponent, CanvasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ paint-front/src/app/canvas/canvas.component.ts
@@ -0,0 +1,41 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-canvas',
+  imports: [],
+  templateUrl: './canvas.component.html',
+  styleUrl: './canvas.component.css',
+})
+export class CanvasComponent implements AfterViewInit {
+  isDrawing: boolean = false;
+
+  @ViewChild('canvas', { static: false })
+  canvas!: ElementRef<HTMLCanvasElement>;
+  private canvasCtx!: CanvasRenderingContext2D | null;
+
+  ngAfterViewInit(): void {
+    if (this.canvas) {
+      this.canvasCtx = this.canvas.nativeElement.getContext('2d');
+    }
+  }
+
+  shouldDraw(event: MouseEvent) {
+    if (event.button === 0) this.isDrawing = true;
+  }
+
+  draw(event: MouseEvent) {
+    if (event.buttons !== 1) return;
+    if (!this.canvasCtx) return;
+    if (!this.isDrawing) return;
+
+    const rect = this.canvas.nativeElement.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    this.canvasCtx.fillStyle = '#000000';
+
+    this.canvasCtx.beginPath();
+    this.canvasCtx.arc(x, y, 5, 0, Math.PI * 2);
+    this.canvasCtx.fill();
+  }
+}
